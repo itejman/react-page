c9fb37078414e0d34a7cc01894905e87847aa80c
Ireneusz Tejman <ireneusz.tejman@exorigo-upos.pl>
2019-07-21 22:11:49 +0200

    create root page / componentent

---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css";
+
+class Root extends React.Component {
+    render() {
+        return(
+            <h1>Page React</h1>
+        )
+    }
+}
+
+export default Root;
