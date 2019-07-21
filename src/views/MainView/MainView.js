56604d442dfcceec06aec1095205916ffddb56f2
Ireneusz Tejman <ireneusz.tejman@exorigo-upos.pl>
2019-07-21 23:34:06 +0200

    Create all page and navbar

---
+++ src/views/MainView/MainView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainView = () => (
+    <h1>Page - MainView</h1>
+)
+
+export default MainView;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import "./index.css";
-
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
+import MainView from '../MainView/MainView';
+import ArticlesView from '../ArticlesView/ArticlesView';
+import BooksView from '../BooksView/BooksView';
+import VideosView from '../VideosView/VideosView';
 
 class Root extends React.Component {
     render() {
         return(
             <BrowserRouter>
-                <>
-                    <Router path="/" />
-                    <Router path="/articles" />
-                    <Router path="/books" />
-                    <Router path="/videos" />
-                </>
+                <Route path="/" component={ NavBar } />
+                <Switch>
+                    <Route exact path="/" component={ MainView } />
+                    <Route path="/articles" component={ ArticlesView } />
+                    <Route path="/books" component={ BooksView } />
+                    <Route path="/videos" component={ VideosView } />
+                </Switch>
             </BrowserRouter>
         )
     }
