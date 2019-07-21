72f22d55afd6542e4718480523b81c819fc3ef94
Ireneusz Tejman <ireneusz.tejman@exorigo-upos.pl>
2019-07-21 22:31:23 +0200

    Add router and view- articles, books, videos

---
+++ src/views/BooksView/BooksView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BooksView = () => (
+    <h1>Page - BooksView</h1>
+)
+
+export default BooksView;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./index.css";
 
+import { BrowserRouter, Router } from 'react-router-dom';
+
 class Root extends React.Component {
     render() {
         return(
-            <h1>Page React</h1>
+            <BrowserRouter>
+                <>
+                    <Router path="/" />
+                    <Router path="/articles" />
+                    <Router path="/books" />
+                    <Router path="/videos" />
+                </>
+            </BrowserRouter>
         )
     }
 }
