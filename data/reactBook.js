d01a959b4fcd6a9327f7d6f6e2bf377288ec3186
Ireneusz Tejman <ireneusz.tejman@exorigo-upos.pl>
2019-07-22 11:11:16 +0200

    JSON | Add to data reackBook

---
+++ data/reactBook.js
@@ -0,0 +1,50 @@
+export const reactBook = [
+    {
+        title: 'The Road to learn React',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/41gYvqhm2yL.jpg',
+        prices: '24.59 USD',
+        isbn: '1491954620',
+        description: 'The Road to learn React teaches you the fundamentals of React. You will build a real world application along the way in plain React without complicated tooling. Everything from project setup to deployment on a server will be explained. The book comes with additional referenced reading material and exercises with each chapter. After reading the book, you will be able to build your own applications in React. The material is kept up to date by me and the community.',
+        link: 'https://www.amazon.com/Road-learn-React-pragmatic-React-js-ebook/dp/B077HJFCQX'
+    },
+    {
+        titile: 'Learning React',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51Q43WRXJzL._SX379_BO1,204,203,200_.jpg',
+        prices: '30.74 USD',
+        isbn: '1491954620',
+        description: 'If you want to learn how to build efficient user interfaces with React, this is your book. Authors Alex Banks and Eve Porcello show you how to create UIs with this small JavaScript library that can deftly display data changes on large-scale, data-driven websites without page reloads. Along the way, you’ll learn how to work with functional programming and the latest ECMAScript features.',
+        link: 'https://www.amazon.com/Learning-React-Functional-Development-Redux-ebook/dp/B071HB1526/'
+    },
+    {
+        titile: 'React: Up & Running',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51hXcWkQIBL._SX379_BO1,204,203,200_.jpg',
+        prices: '22.57 USD',
+        isbn: '1491931825',
+        description: 'Hit the ground running with React, the open-source technology from Facebook for building rich web applications fast. With this practical guide, Facebook engineer Stoyan Stefanov teaches you how to build components--React basic building blocks--and organize them into maintainable, large-scale apps.',
+        link: 'https://www.amazon.com/React-Running-Building-Web-Applications-ebook/dp/B01IG0IH2S'
+    },
+    {
+        titile: 'Learning React Native',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/518ctEpEw1L._SX379_BO1,204,203,200_.jpg',
+        prices: '36.89 USD',
+        isbn: '1491989149',
+        description: 'Get a practical introduction to React Native, the JavaScript framework for writing and deploying fully featured mobile apps that render natively. The second edition of this hands-on guide shows you how to build applications that target iOS, Android, and other mobile platforms instead of browsers—apps that can access platform features such as the camera, user location, and local storage.',
+        link: 'https://www.amazon.com/Learning-React-Native-Building-JavaScript-ebook/dp/B076PWRYSS'
+    },
+    {
+        titile: 'React Native Cookbook',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51j3Rdi8ciL._SX379_BO1,204,203,200_.jpg',
+        prices: '22.57 USD',
+        isbn: '1491993847',
+        description: 'Tackling an app development project on multiple platforms is no simple task. When time is in short supply and customers need access from the tap of a home screen, React Native can provide a lean development team with the tools needed to deliver a multi-platform native experience without juggling multiple programming languages and shifting code bases. React Native is an emerging technology and best practices are only beginning to bubble up.',
+        link: 'https://www.amazon.com/React-Native-Cookbook-Bringing-Platforms-ebook/dp/B079STK39H'
+    },
+    {
+        titile: 'Learning React and Redux',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51AFwrzNmdL._SX386_BO1,204,203,200_.jpg',
+        prices: '21.24 USD',
+        isbn: '013484355X',
+        description: 'As far as new web frameworks and libraries go, React is quite the runaway success. It not only deals with the most common problems developers face when building complex apps, it throws in a few additional tricks that make building the visuals for such apps much, much easier.',
+        link: 'https://www.amazon.com/Learning-React-Hands-Building-Applications-ebook/dp/B07CQPL2BM'
+    },
+]
